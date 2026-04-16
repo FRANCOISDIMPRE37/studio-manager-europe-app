cdcd901ce8a8f01671fa45afc07e153cf6562d73
FRANCOISDIMPRE37 <contact@intemporelle.eu>
2026-04-16 02:27:54 +0200

    Add JWT_SECRET to pm2 configuration

--- pm2.config.js
+++ pm2.config.js
@@ -1,13 +1,1 @@
-module.exports = {
-  apps: [{
-    name: 'studio-manager',
-    script: './dist/index.js',
-    cwd: '/home/ubuntu/app',
-    env: {
-      NODE_ENV: 'production',
-      PORT: '8080',
-      DATABASE_URL: 'mysql://studiomanager:StudioOvh2026!@bo810531-001.eu.clouddb.ovh.net:35120/studiomanager?ssl={"rejectUnauthorized":false}',
-      JWT_SECRET: '',
-    }
-  }]
-}
+JWT_SECRET:'2ce97c70e957dd7d29c6f95ec185d45dc35746ac46dca5d9e2f429e9b9cc370a52afe28e58ccf71afc6c6389810e7ed486e4d20573d4a6b9d0d4f95038931f53',
